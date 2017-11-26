78cbd48b1fe20745d7b42bc6e535d091af3218bc
jtraynor <traynor.jack@gmail.com>
2017-11-26 23:30:14 +1100

    Added benchmark tests

---
+++ prime_bench_test.go
@@ -0,0 +1,39 @@
+package prime_test
+
+import (
+	"testing"
+
+	"github.com/jtraynor/prime"
+)
+
+func BenchmarkGenerate(b *testing.B) {
+	short := testing.Short()
+
+	for name, tc := range checksumTT {
+		if short && !tc.isShort {
+			continue
+		}
+
+		b.Run(name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				primes := make(chan uint64, tc.limit/1000)
+				go prime.Generate(primes, tc.limit)
+
+				sum := uint64(0)
+				for number := range primes {
+					sum += number
+				}
+			}
+		})
+	}
+}
+
+func BenchmarkIsPrime(b *testing.B) {
+	for name, tc := range isPrimeTT {
+		b.Run(name, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				prime.IsPrime(tc.candidate)
+			}
+		})
+	}
+}
